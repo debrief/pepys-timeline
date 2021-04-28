61a4f2e853afe2373f71703b002ca338ff080716
krsnik93 <ivankrsnik93@gmail.com>
2021-04-28 19:28:32 +0100

    fix coverage formula

--- pepys_timeline/static/js/src/pepys.js
+++ pepys_timeline/static/js/src/pepys.js
@@ -178,15 +178,11 @@ function transformParticipant(participant, serial) {
         ]));
     participant.coverage = periods;
 
-    const totalParticipation = participantStats
-        .map(s => new Date(s.resp_end_time) - new Date(s.resp_start_time))
-        .reduce((s, d) => s + d, 0);
+    const totalParticipation = (new Date(serial.end) - new Date(serial.start)) || 0;
     const totalCoverage = participantStats
-        .filter(s => s.resp_range_type === "C")
         .map(s => new Date(s.resp_end_time) - new Date(s.resp_start_time))
         .reduce((s, d) => s + d, 0);
-    participant["percent-coverage"] = totalParticipation !== 0 ? totalCoverage / totalParticipation : 0;
-
+    participant["percent-coverage"] = totalParticipation !== 0 ? 100 * totalCoverage / totalParticipation : 0;
     participant["platform-type"] = participant["platform_type_name"];
 
     return {
@@ -202,7 +198,7 @@ function transformParticipant(participant, serial) {
             background_color: participant["force_type_color"]
         },
         percentage: {
-            measure: participant["percent-coverage"] + " %",
+            measure: participant["percent-coverage"].toFixed(2) + " %",
             class: "ypercentage_" + calculatePercentageClass(participant["percent-coverage"])
         },
         data: periods
