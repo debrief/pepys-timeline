6ba04a4a2ee509b8d9e5260235814c280b8ed2b2
krsnik93 <ivankrsnik93@gmail.com>
2021-04-29 17:50:12 +0100

    fix coverage formula pt 5

--- pepys_timeline/static/js/src/pepys.js
+++ pepys_timeline/static/js/src/pepys.js
@@ -183,8 +183,8 @@ function transformParticipant(participant, serial) {
         .map(s => new Date(s.resp_end_time) - new Date(s.resp_start_time))
         .reduce((s, d) => s + d, 0);
     const totalCoverage = participantStats
-        .map(s => new Date(s.resp_end_time) - new Date(s.resp_start_time))
         .filter(s => s.resp_range_type === "C")
+        .map(s => new Date(s.resp_end_time) - new Date(s.resp_start_time))
         .reduce((s, d) => s + d, 0);
     participant["percent-coverage"] = totalParticipation !== 0 ? 100 * totalCoverage / totalParticipation : 0;
     participant["platform-type"] = participant["platform_type_name"];
